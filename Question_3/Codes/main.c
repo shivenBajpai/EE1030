13ac05ea443194b5e4b3a1d06f87407cd60bc6d4
shivenBajpai <shiven.bajpai@gmail.com>
2024-08-30 10:37:45 +0530

    Q3 Code Revision

--- Question_3/Codes/main.c
+++ Question_3/Codes/main.c
@@ -1,16 +1,15 @@
-// Loads a series of random points and also calculates the LHS and RHS or the equation in the question
-int get_data(float *array) {
-    array[0] = 1.0;  
-    array[1] = 2;
+typedef struct {
+    float x, y;
+} point;
 
-    array[2] = 2;  
-    array[3] = 4;  
-    
-    array[4] = 3;  
-    array[5] = 6;
-    
-    array[6] = (array[3]-array[1])/(array[2]-array[0]);
-    array[7] = (array[5]-array[3])/(array[4]-array[2]);
+// Loads a series of random points and also calculates the LHS and RHS or the equation in the question
+int get_data(point *points, float *lhs, float *rhs) {
+    (points[0]) = (point) {.x=1., .y=2.};
+    (points[1]) = (point) {.x=2., .y=4.};
+    (points[2]) = (point) {.x=3., .y=6.};
+
+    *lhs = (float) (points[1].y - points[0].y)/(points[1].x - points[0].x);
+    *rhs = (float) (points[2].y - points[1].y)/(points[2].x - points[1].x);
 
     return 0;
 }
