967ea0e4a6985893a8efbbc13dc85b8f006870dc
shivenBajpai <shiven.bajpai@gmail.com>
2024-09-11 23:40:56 +0530

    Q4Rev1

--- Question_4/Codes/main.c
+++ Question_4/Codes/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Struct to represent 3 dimensional vectors
 typedef struct {
     float x;
     float y;
@@ -9,35 +10,37 @@ typedef struct {
     float norm;
 } Vector;
 
+// Generates n random vectors of norm 4
 void generate_random_vectors(Vector* vectors, size_t n, unsigned int seed) {
     
     srand(seed);
     float x,y,z,norm;
 
     for (size_t i = 0; i < n; i++) {
-        x = (((float)rand()*2 / RAND_MAX)-1);
+        x = (((float)rand()*2 / RAND_MAX)-1); // Assign a random value from -1 to 1
         y = (((float)rand()*2 / RAND_MAX)-1);
         z = (((float)rand()*2 / RAND_MAX)-1);
         norm = sqrtf(x*x + y*y + z*z);
         
-        vectors[i].x = (x / norm) * 4;
+        vectors[i].x = (x / norm) * 4; // Adjust norm to 4
         vectors[i].y = (y / norm) * 4;
         vectors[i].z = (z / norm) * 4;
         vectors[i].norm = 4;
     }
 }
 
+// Scales each vector by a random lambda (rerolled for each vector) between -2 to 3
 void scale_vectors(Vector* in_vectors, Vector* out_vectors, size_t n) {
 
     float lambda;
     
     for (size_t i = 0; i < n; i++) {
-        lambda = ((float)rand()*5/RAND_MAX)-3;
+        lambda = ((float)rand()*5/RAND_MAX)-3; // Random value between -2 and 3
 
-        out_vectors[i].x = in_vectors[i].x * lambda;
+        out_vectors[i].x = in_vectors[i].x * lambda; // Scale the vector
         out_vectors[i].y = in_vectors[i].y * lambda;
         out_vectors[i].z = in_vectors[i].z * lambda;
-        out_vectors[i].norm = sqrtf(pow(out_vectors[i].x, 2) + pow(out_vectors[i].y, 2) + pow(out_vectors[i].z, 2));
+        out_vectors[i].norm = sqrtf(pow(out_vectors[i].x, 2) + pow(out_vectors[i].y, 2) + pow(out_vectors[i].z, 2)); // Recalculate norm
     }
 }
 
@@ -51,4 +54,3 @@ Vector** get_data(int n) {
 
     return (Vector*[2]){vectors, scaled_vectors};
 }
-
