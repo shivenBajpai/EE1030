cdb408fffd3d8556fd4667c02b6bc0bd4dac7e48
shivenBajpai <shiven.bajpai@gmail.com>
2024-09-16 18:39:18 +0530

    Q5rev2

--- Question_5/Codes/main.py
+++ Question_5/Codes/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 lib = ctypes.CDLL('./libvectors.so')
 
-def draw_angle_between_vectors(v1, v2, ax, angle, text_offset=0):
+def draw_angle_between_vectors(v1, v2, ax, text_offset=0):
     # Normalize the vectors
     v1 = v1 / np.linalg.norm(v1)
     v2 = v2 / np.linalg.norm(v2)
@@ -28,24 +28,30 @@ def draw_angle_between_vectors(v1, v2, ax, angle, text_offset=0):
 
     # Label the angle in the middle of the arc
     mid_arc_point = arc_points[len(arc_points) // 2] + (v1+v2)/5
-    ax.text(mid_arc_point[0]+text_offset, mid_arc_point[1], mid_arc_point[2], f'{angle:.0f}°', color='purple', fontsize=9)
+    ax.text(mid_arc_point[0]+text_offset, mid_arc_point[1], mid_arc_point[2], f'{angle_deg:.0f}°', color='purple', fontsize=9)
     
 
 # Define the Vector struct in Python
 class Vector(ctypes.Structure):
-    _fields_ = [("x", ctypes.c_float),
-                ("y", ctypes.c_float),
-                ("z", ctypes.c_float),
-                ("norm", ctypes.c_float)]
+    _fields_ = [("x", ctypes.c_double),
+                ("y", ctypes.c_double),
+                ("z", ctypes.c_double)]
 
 
 # Specify the return type of the get_vector function
-lib.get_vector.restype = ctypes.POINTER(Vector)
+lib.calculate_cosines.restype = ctypes.POINTER(Vector)
+lib.calculate_cosines.argtypes = [ctypes.c_double, ctypes.c_double] 
 
 # Call the C function
-vector_ptr = lib.get_vector();
+vector_ptr = lib.calculate_cosines(60,45);
 origin = np.array([0,0,0]);
 vector = np.array([vector_ptr[0].x, vector_ptr[0].y, vector_ptr[0].z])*2; # All vectors are scaled by 2 to improve clarity of plot
+
+print("Taking angle between \nvector and X-axis as alpha = 60 degrees\nvector and Z-axis as gamma = 45 degrees\n")
+print("Found cosines to be:")
+print("Cos alpha:", vector_ptr[0].x)
+print("Cos beta:", vector_ptr[0].y)
+print("Cos gamma:", vector_ptr[0].z)
 
 # Plot the Vector
 fig = plt.figure()
@@ -56,9 +62,9 @@ ax.quiver(*origin, 0, 2, 0, length=1, color='k')
 ax.quiver(*origin, 2, 0, 0, length=1, color='k')
 
 # Draw the angle arcs
-draw_angle_between_vectors(np.array([0,0,1]),vector,ax,45,0.2)
-draw_angle_between_vectors(np.array([0,1,0]),vector,ax,45)
-draw_angle_between_vectors(np.array([1,0,0]),vector,ax,60)
+draw_angle_between_vectors(np.array([0,0,1]),vector,ax,0.2)
+draw_angle_between_vectors(np.array([0,1,0]),vector,ax)
+draw_angle_between_vectors(np.array([1,0,0]),vector,ax)
 
 ax.set_xlim([-2, 2])
 ax.set_ylim([-2, 2])
